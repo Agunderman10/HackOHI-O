5dc27ee750e51e98b92ecea1b41354ebacd67b9a
Salil Monga <monga.monga43@gmail.com>
2019-11-02 13:27:14 -0400

    created pages and profile.

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do you want to learn React?
         </a>
       </header>
     </div>
