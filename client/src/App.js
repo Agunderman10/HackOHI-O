cfad6a5785729db92b1e486be13ce5bd5b37dcbc
Salil Monga <monga.monga43@gmail.com>
2019-11-02 21:05:05 -0400

    Added Profile ,fixed server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Profile from './Profile.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit src/App.js and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Do you want to learn React?
-        </a>
-      </header>
-    </div>
+    
+          <Profile/>
+      
+      </div>
   );
 }
 
---
+++ client/src/Profile.js
@@ -0,0 +1,29 @@
+import React,{useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './profile.css';
+function Profile() {
+  const [user,setUser]= useState({});
+  useEffect(()=>{
+    getData();
+  },[])
+// getting data and storing it into getData.
+  const getData =async ()=>{
+    const response=await fetch(`http://localhost:8080/coder-profile/1`)
+    const data=await response.json(); 
+    console.log(data);
+    setUser(data);
+  }
+
+  return (    
+        <div className="w3-display-container">
+          <img src={user.coder_profile_pic} alt="Avatar"/>
+            <h2>{user.coder_name}</h2>
+          <p><i className></i>{user.coder_bio}</p>
+          <p><i className></i>{user.coder_location}</p>
+          <p><i className></i>{user.coder_email}</p>
+          <p><i className></i>1224435534</p>
+  </div>
+  );
+}
+
+export default Profile;
